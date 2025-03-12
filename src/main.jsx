11a01892161c418229b75d02b45baf023b34cf26
CristianGC006 <cogolloguzmanesteban@gmail.com>
2025-03-12 16:10:17 -0500

    (Feat)Contenido:Hemos creado el enrutado y encarpetado basico

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import PanelDeControl from './pages/PanelDeControl'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Login from './pages/auth/login'
+let router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Login />
+  },
+  {
+    path:'/panel',
+    element: <PanelDeControl />
+  }
+])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PanelDeControl />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
